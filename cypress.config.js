5dfe9410304cb46202fe795d5f17d0e93dbc5a5a
sonuap <sonu@assessprep.com>
2022-12-11 12:10:21 +0530

    working file for me

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 10000,
+  defaultCommandTimeout: 20000,
   // chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
